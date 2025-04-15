2868012748bb2c0b484daab558688a3437d1320f
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-04-15 16:53:29 -0300

    Delete ExerciciosLista directory

--- ExerciciosLista/exercicio9.py
+++
@@ -1,9 +0,0 @@
-soma = 0
-
-for numero in range(1,10001):
-    for i in range(1,numero):
-        if numero % i == 0:
-            soma += i
-    if soma == numero:
-        print(f"O número {numero} é perfeito!")
-    soma -= soma
